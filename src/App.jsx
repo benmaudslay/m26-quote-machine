af20ba0a1b09aed284023a26ae9f2c8e893f08e8
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-959.local>
2021-08-03 10:52:50 +0100

    fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 const App = () => {
+
+  const handleFetch = async () => {
+    const response = await fetch("https://api.chucknorris.io/jokes/random")
+    const data = await response.json()
+    console.log(data)
+    // fetch("https://api.chucknorris.io/jokes/random")
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))
+  }
+
   return (
     <div>
       <h1>Hi there</h1>
+      <button onClick={handleFetch}>click me</button>
     </div>
   )
 }
